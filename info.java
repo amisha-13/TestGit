e058a35cb782fbd98450e5a4832fa15a2d99ee64
amisha-13 <amishajoshi2713@gmail.com>
2020-06-04 12:40:12 +0530

    Submaster info.java committed

--- info.java
+++ info.java
@@ -1,7 +1,8 @@
-git addclass info
+class info
 {
 public static void main(String args[])
 {
+System.out.println("Welcome to github!");
 System.out.println("Create you own github account!");
 System.out.println("Make sure you create a strong password!");
 System.out.println("My github username is amisha-13");
